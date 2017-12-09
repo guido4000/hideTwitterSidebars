0e66c8dd62509288a918ebc957c92b7620c9a03f
Guido <guido4000@gmail.com>
2017-12-09 17:17:06 +0100

    Initial commit

---
+++ hideSidebar.js
@@ -0,0 +1,5 @@
+var elementLeft = document.getElementsByClassName("dashboard-left");
+elementLeft[0].style.visibility = "hidden";
+
+var elementRight = document.getElementsByClassName("dashboard-right");
+elementRight[0].style.visibility = "hidden";
---
+++ zip.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+zip -r HideTwitterSidebars.zip . -x "*.DS_Store" -x "zip.sh"
