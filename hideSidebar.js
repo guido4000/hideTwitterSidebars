7584c1af09280811c2bcb69b03c31c372bcf594f
Guido <guido4000@gmail.com>
2017-12-11 23:52:29 +0100

    Center main div

--- hideSidebar.js
+++ hideSidebar.js
@@ -1,5 +1,19 @@
-var elementLeft = document.getElementsByClassName("dashboard-left");
-elementLeft[0].style.visibility = "hidden";
+init();
 
-var elementRight = document.getElementsByClassName("dashboard-right");
-elementRight[0].style.visibility = "hidden";
+function init() {
+  document.getElementsByTagName("html")[0].style.visibility = "hidden";
+
+  document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementsByTagName("html")[0].style.visibility = "initial";
+
+    var elementLeft = document.getElementsByClassName("dashboard-left");
+    elementLeft[0].style.display = "none";
+
+    var elementRight = document.getElementsByClassName("dashboard-right");
+    elementRight[0].style.visibility = "hidden";
+
+    var elementMain = document.getElementsByClassName("content-main");
+    elementMain[0].style.float = "unset";
+    elementMain[0].style.margin = "0 auto";
+  });
+}
