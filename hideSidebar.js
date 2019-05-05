5c99c6d2cf9f4f9ae3cf50088e7dd6d3c2fedfd5
guido4000 <guido4000@gmail.com>
2019-05-05 12:17:25 +0200

    adjust to new Twitter design

--- hideSidebar.js
+++ hideSidebar.js
@@ -9,8 +9,16 @@ function init() {
     var elementLeft = document.getElementsByClassName("dashboard-left");
     elementLeft[0].style.display = "none";
 
+    var elementLeftNew = document.querySelectorAll("header");
+    elementLeftNew[0].style.display = "none";
+
     var elementRight = document.getElementsByClassName("dashboard-right");
     elementRight[0].style.visibility = "hidden";
+
+    var elementRightNew = document.querySelectorAll(
+      '[data-testid="sidebarColumn"]'
+    );
+    elementRightNew[0].style.visibility = "hidden";
 
     var elementMain = document.getElementsByClassName("content-main");
     elementMain[0].style.float = "unset";
